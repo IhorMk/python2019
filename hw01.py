6010e9e33db4281c6c67654d6727b21f195087aa
Ihor Blok <ihorblok@gmail.com>
2019-12-13 00:58:58 +0200

    fix

--- hw01.py
+++ hw01.py
@@ -3,6 +3,6 @@ import re
 import sys
 spys=[]
 with open("/Book.txt", "r") as f:
-    spys=collections.Counter(sorted(re.findall(r'(\w*[\'\-\w+]\w+)', f.read().lower())))
+    spys=collections.Counter(sorted(re.findall(r'(\w+[\'\-\w+]\w+)', f.read().lower())))
     for i in spys:
-        print ('Word ', i, 'is placed in text', str(spys[i]), 'times')
+        print ('Word', i, 'is placed in text', str(spys[i]), 'times')
